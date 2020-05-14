9b1a750496714a570feb591d9648d4c52f9461c7
Laurențiu Andronache <laurentiu.andronache@trailung.ro>
2020-05-14 17:27:35 +0100

    Initial commit

---
+++ tests/test_skeleton.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from cirr.skeleton import fib
+
+__author__ = "LaurenÈ›iu Andronache"
+__copyright__ = "LaurenÈ›iu Andronache"
+__license__ = "mit"
+
+
+def test_fib():
+    assert fib(1) == 1
+    assert fib(2) == 1
+    assert fib(7) == 13
+    with pytest.raises(AssertionError):
+        fib(-10)
